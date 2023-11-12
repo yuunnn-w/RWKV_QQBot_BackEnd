2872fc615ea58d5ee408ef8dca8d5d7c1aa0039a
jiaXinSugar <jiaxinsugar@gmail.com>
2023-11-12 13:18:48 +0800

    优化代码逻辑，优化配置文件

--- bot.py
+++ bot.py
@@ -18,6 +18,7 @@ CONSERVATION = ROLE_CONFIG.get('Conservation')
 PROMPT = read_prompt(read_config("Prompt", "ROLE_PROMPT")).strip()
 CONSERVATION[0]["content"] = PROMPT
 NAMES = ROLE_CONFIG.get("Role")
+CONTEXT_LENGTH = int(read_config("Prompt", "CONTEXT_LENGTH"))#上下文长度
 
 #OpenAI接口设置
 URL = read_config("OpenAI", "URL")
@@ -27,7 +28,7 @@ TOKEN_COUNT = read_config("OpenAI", "TOKEN_COUNT")
 #Shamrock框架交互设置
 SHAMROCK_IP = read_config("Shamrock", "IP")
 SHAMROCK_PORT = read_config("Shamrock", "PORT")
-QQ_AT = read_config("Shamrock", "QQ_AT")
+QQ = read_config("Shamrock", "QQ") #这里需要你在配置文件中改成你挂shamrock的QQ号
 
 #Model模型设置
 MAX_TOKENS = int(read_config("Model", "MAX_TOKENS"))
@@ -38,7 +39,7 @@ FREQUENCY_PENALTY = float(read_config("Model", "FREQUENCY_PENALTY"))
 PENALTY_DECAY = float(read_config("Model", "PENALTY_DECAY"))
 MODEL = read_config("Model", "MODEL")
 
-Private_Lock = False #全局锁
+Private_Lock = False #全局私聊锁
 conversation = copy.deepcopy(CONSERVATION)
 
 @app.route('/', methods=['POST'])
@@ -49,32 +50,36 @@ def handle_post_request():
     global NAMES
     
     data = request.get_json()
+    #print(data)
     if data is not None:
         try:
             cq_codes, message = parse_cq_codes(data['message'])
         except:
             return 'OK', 200
         
+        message_id = data['message_id']
         message_type = data['message_type']
+        
         if message_type == 'private':
             target_id = data['user_id']
-            if message.strip() == 'lock on':
+            if message.strip() == 'lock on':#如果接收到lock on的私聊消息，就开启私聊锁，此时只能进行私聊对话
                 Private_Lock = True
                 send_msg('LOCK ON', message_type, target_id)
                 return 'OK', 200
-            elif message.strip() == 'lock off':
+            elif message.strip() == 'lock off':#关闭私聊锁
                 Private_Lock = False
                 send_msg('LOCK OFF', message_type, target_id)
                 return 'OK', 200
+            
         elif message_type == 'group':
-            if (QQ_AT not in cq_codes) or Private_Lock:
-                if Private_Lock and QQ_AT in cq_codes:
+            if (f'[CQ:at,qq={QQ}]' not in cq_codes) or Private_Lock:
+                if Private_Lock and f'[CQ:at,qq={QQ}]' in cq_codes:
                     send_msg('很抱歉，管理员已暂停本AI服务，请联系管理员解除锁定。', message_type, data['group_id'])
                 return 'Pause Service!', 200
             else:
                 target_id = data['group_id']
                 
-        if message.strip() == 'clean' or len(conversation) > 50:
+        if message.strip() == 'clean' or len(conversation) > CONTEXT_LENGTH:
             conversation = copy.deepcopy(CONSERVATION)
             send_msg('已成功清除上下文！', message_type, target_id, ip=SHAMROCK_IP, port=SHAMROCK_PORT)
             return 'Done', 200
@@ -106,6 +111,8 @@ def handle_post_request():
         
         else:
             conversation.append({"role": "user", "content": message})
+            #如果需要对接OpenAI api接口，需要删除一些参数，需要删掉"stop"和"penalty_decay"以及"names"
+            #"stop"、"penalty_decay"、"names"这三个参数是ai00 rwkv server特有的
             payload = {
                 "messages": conversation,
                 "max_tokens": MAX_TOKENS,
@@ -117,16 +124,17 @@ def handle_post_request():
                 "stop": ["\n\n","\nUser:","User:"],
                 "stream": False,
                 "model": MODEL,
-                "names": NAMES,
+                "names": NAMES, #该参数用于映射对话人物姓名，加深催眠效果
                 #"logit_bias":{261:-1}
             }
-            print(payload)
+            #print(payload)
             """
             proxy_dict = {
                 "http": "http://127.0.0.1:7890",
                 "https": "http://127.0.0.1:7890",
             }
             """
+            #暂时废弃掉的代理
             response = call_openai_api(payload, URL, API_KEY)#, proxy_dict)
             if "error" in response:
                 return_message = f"Error: {response['error']}".strip()
@@ -135,15 +143,19 @@ def handle_post_request():
                 return jsonify(response), 200
             else:
                 return_message = f"{response['choices'][0]['message']['content']}".strip()
-                print("RWKV5:", return_message)
-                token_usage = int(response.get('usage').get('total_tokens'))
+                print("RWKV5:", return_message) #打印模型返回信息
+                token_usage = int(response.get('usage').get('total_tokens')) #这里计算消耗的token数目
                 update_token_count(file_name = TOKEN_COUNT, token_count = token_usage)
                 #计算token
 
             # Append the assistant's response to the conversation
             conversation.append({"role": "assistant", "content": return_message})
-            response = send_msg(return_message, message_type, target_id, ip=SHAMROCK_IP, port=SHAMROCK_PORT)
-            #发送消息
+            if message_type == 'private':#回复私聊不需要reply
+                reply = ''
+            else:
+                reply = f'[CQ:reply,id={message_id}]'
+            response = send_msg(reply+return_message, message_type, target_id, ip=SHAMROCK_IP, port=SHAMROCK_PORT)
+            #向Shamrock发送消息
             return "Successfully Send!", 200
     else:
         return "Invalid JSON data received", 400  # Return a 400 Bad Request response if the data is not valid JSON
